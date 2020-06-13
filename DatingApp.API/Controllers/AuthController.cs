b396d014c24cc629fba00b0c6a9c0d1fffcf54e6
famantanantsoaandry <a.famantanantsoa@gmail.com>
2020-06-13 13:47:27 +0200

    seection 5 : error handders both API and SPA

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -52,8 +52,6 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-           
-            throw new Exception("computer says no!");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
